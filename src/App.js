979b9ec382de62f57d7077f89e9abb9b709dd521
Aneesh Saripalli <aneeshsaripalli@gmail.com>
2020-05-26 19:40:29 -0500

    Changed app text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          feature change
+          Learn React tomorrow
         </a>
       </header>
     </div>
